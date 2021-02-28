62a541cf3f474d16bcec7e7c80e4c073d6d7aca8
tshalev <tomer.shalev@gmail.com>
2021-02-28 18:56:17 +0200

    added gh action

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,3 @@
-import matter from 'gray-matter'
-import Link from 'next/link'
-import path from 'path'
-import Layout from '../src/components/Layout'
-import SideBar from '../src/components/side-bar'
-import Header from '../src/components/header'
-import { useColorMode, Switch, Stack, Box, BoxProps } from '@chakra-ui/react'
-
 export default function Index( props ) {
   const { name, groups } = props
   return (
