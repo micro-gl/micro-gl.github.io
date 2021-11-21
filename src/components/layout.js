828e85bb6451cccdd0e129ec3f651b9f10230fd8
tshalev <tomer.shalev@gmail.com>
2021-11-21 23:27:40 +0200

    minor improvements

--- src/components/layout.js
+++ src/components/layout.js
@@ -56,9 +56,9 @@ export default function Layout( props ) {
       { 
         // <Fade in={menu} out={!menu}>bg={colors(is_dark_mode).bg}
         <Slide in={menu} direction="right" 
-            style={{ zIndex: 100, position: 'absolute', top: headerHeight, left: '0px', width:"100%", height:"100vh" }}>
-          {menu && <SideBar px={2} pt={1} bg={colors(is_dark_mode).bg}
-                  w="100%" h="100vh" 
+            style={{ zIndex: 100, position: 'absolute', top: headerHeight, left: '0px', width:"100%", height:`calc(100vh - ${headerHeight})` }}>
+          {<SideBar px={2} pt={1} bg={colors(is_dark_mode).bg}
+                  w="100%" h='100%'
                   groupFontSize="1.2rem" itemFontSize="1.0rem"
                   selectedSlug={slug}
                   groups={groups} name={name} onSelect={toggleMenu}  />}
